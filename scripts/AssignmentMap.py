c44247a3fbfb992eb453bb8669cecc2fa18bf756
RaymondKirk <ray.tunstill@live.co.uk>
2017-03-21 02:43:38 +0000

    Added mapping to search at predetermined points

--- scripts/AssignmentMap.py
+++ scripts/AssignmentMap.py
@@ -9,6 +9,7 @@ from sensor_msgs.msg import LaserScan
 from cv_bridge import CvBridge
 from geometry_msgs.msg import Twist, PoseStamped, PoseWithCovarianceStamped
 from move_base_msgs.msg import MoveBaseActionFeedback
+from actionlib_msgs.msg import GoalID
 
 class assignment_map:
     
@@ -42,6 +43,11 @@ class assignment_map:
     amcl = []
     feedback_data = []
     
+    points = [[-2, -4, 0.004], [-0.9, 3.15, 0.007], [1.15, -0.5, -0.001], [-4, 1.53, 0.004]]
+    current_point = 0
+    searching = False
+    found_an_object = False
+    
     def __init__(self):
         cv2.namedWindow("Image window", 1)
         cv2.namedWindow("Thresh", 1)
@@ -49,10 +55,12 @@ class assignment_map:
         cv2.startWindowThread()
         self.motion_pub = rospy.Publisher("/turtlebot/cmd_vel", Twist, queue_size=10)
         self.move_base_pub = rospy.Publisher("turtlebot/move_base_simple/goal", PoseStamped, queue_size=10)
+        self.cancel_move_base_pub = rospy.Publisher("/turtlebot/move_base/cancel", GoalID, queue_size=10)        
         self.move_base_feedback_pub = rospy.Subscriber("turtlebot/move_base/feedback", MoveBaseActionFeedback, self.update_feedback)        
         self.laser_sub = rospy.Subscriber("/turtlebot/scan/", LaserScan, self.update_laser_data)
         self.image_sub = rospy.Subscriber("/turtlebot/camera/rgb/image_raw", Image, self.image_raw)
         self.position_sub = rospy.Subscriber("/turtlebot/amcl_pose", PoseWithCovarianceStamped, self.amcl_pose)        
+        rospy.sleep(2)
         
     def update_laser_data(self, data):
         self.laser_data = data.ranges
@@ -76,7 +84,9 @@ class assignment_map:
         #mask_img[0:self.h/4, 0:self.w] = 0
         #mask_img[self.h - (self.h/4):self.h, 0:self.w] = 0
         
-        #self.naive_behaviour()        
+        if self.searching:
+            self.naive_behaviour()
+            
         self.display(cv_image)
     
     def amcl_pose(self, data):
@@ -114,6 +124,9 @@ class assignment_map:
         a = (c_l - c_r) / self.robot_radius
         return (v, a)   
     
+    def found_all(self):
+        return self.foundRed and self.foundGreen and self.foundBlue and self.foundYellow
+        
     def check_object(self, image):
         mid_x = int(round(self.w/2))
         mid_y = int(round(self.w/2))
@@ -139,30 +152,88 @@ class assignment_map:
             
         return False
     
-    def move_to(self, x, y, orientation):
+    def move_to(self, x, y, z):
         ps = PoseStamped()
         ps.header.frame_id = "/map"
         ps.header.stamp = rospy.Time.now()
         ps.pose.position.x = x
         ps.pose.position.y = y
         ps.pose.position.z = 0
-        ps.pose.orientation = orientation
+        ps.pose.orientation = self.amcl.pose.pose.orientation
         self.move_base_pub.publish(ps)
+    
+    def move_next(self):
+        self.found_an_object = False
+        if self.current_point == len(self.points):
+            self.current_point = 0
+        
+        i = self.current_point
+        x = 0
+        y = 1
+        z = 2
+        
+        self.move_to(self.points[i][x], self.points[i][y], self.points[i][z])
+        
+    def is_at_position(self):
+        pos = self.amcl.pose.pose.position
+        i = self.current_point 
+        
+        x = 0
+        y = 1
+        z = 2
+
+        try:
+            target_x = self.points[i][x]
+            target_y = self.points[i][y]
+            target_z = self.points[i][z]
+            
+            x_diff = abs(pos.x - target_x)
+            y_diff = abs(pos.y - target_y)
+            z_diff = abs(pos.z - target_z)
+            
+            threshold = 0.3
+            at_pos = x_diff <= threshold and y_diff <= threshold and z_diff <= threshold
+            
+            if(at_pos):
+                print "I'm at the positon"
+                self.current_point += 1
+            
+            return at_pos
+        except Exception:
+            return False
+    
+    def cancel_move_base(self):
+        a = GoalID()
+        self.cancel_move_base_pub.publish(a)
+    
+    def object_search(self):    
+        self.searching = True
+        return True
+        
+    def go_to_next(self):
+        if(self.found_an_object):
+            return True
+        else:
+            self.found_an_object = False
+            return False
         
     def found_object(self):
         m = Twist()
         m.linear.x = m.linear.y = m.angular.z = 0
         self.motion_pub.publish(m)
         print "Found object!"
+        self.found_an_object = True
+        self.searching = False
         rospy.sleep(2)
         return True
 
     def naive_behaviour(self):
+        print "Behaviour on"
         v = 0 #Set initial velocity and angular
         a = 0
         
-        v_speed = 1 #Set variaboles for v, a speeds
-        a_speed = 0.8
+        v_speed = 0.8 #Set variaboles for v, a speeds
+        a_speed = 0.6
         d_min = 1 #Set a variable for min distance to get to objects
         
         cmd_vel = Twist()
@@ -183,8 +254,6 @@ class assignment_map:
         
         left_average = numpy.mean(left_to_mid)
         right_average = numpy.mean(mid_to_right)
-        
-        print left_range, center_range, right_range
                     
         if(self.mask_img.sum() >= 1):
             M = cv2.moments(self.mask_img)
@@ -197,13 +266,6 @@ class assignment_map:
                 v = v_speed
                 a = (-float(cx - self.w/2) / 100)
                 
-        elif left_range > d_min and right_range > d_min:
-            direction_min = min([left_range, center_range, right_range])
-            v = v_speed if direction_min > d_min else -v_speed
-        elif left_range - right_range < -d_min:
-            a = a_speed
-        elif left_range - right_range > +d_min:
-            a = -a_speed
         else:
             a = a_speed
         
@@ -215,10 +277,23 @@ class assignment_map:
 def main():
     rospy.init_node('assignment_one')
     tb = assignment_map()
-    pos = [0, 0]
-
-    while(True):
-        print tb.feedback_data
+    
+    sentGoal = False    
+    while(not tb.found_all()):
+        if(sentGoal == False):
+            print "Sending goal"
+            tb.move_next()
+            sentGoal = True
+            
+        if(tb.is_at_position()):
+            print "At position"
+            tb.cancel_move_base()
+            rospy.sleep(1)
+            tb.object_search()
+        else:
+            print "Searching:", tb.go_to_next()
+            sentGoal = not tb.go_to_next()
+        
         rospy.sleep(1)
     
     rospy.spin()
@@ -226,3 +301,28 @@ def main():
 if __name__ == '__main__':    
     main()
     cv2.destroyAllWindows()
+    
+#        def object_search(self):
+#        self.initial_spin()
+#        return True
+#    
+#    def turn_until(self, i):
+#        m = Twist()
+#        m.linear.x = 0
+#        m.angular.z = 0.5
+#        
+#        if i >= 10:
+#            return True
+#            
+#        if(self.mask_img.sum >= 100):
+#            m.angular.z = 0
+#            self.motion_pub.publish(m)
+#            return True
+#            
+#        rospy.sleep(2)
+#        self.turn_until(i)
+#        
+#
+#    def initial_spin(self):
+#        self.searching = turn_until()
+#        return False
